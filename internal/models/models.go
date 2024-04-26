bfadefe68a4bdcdcb616ecc0d9f7647c7a18272f
Milwad <milwad.dev@gmail.com>
2024-04-26 17:47:50 +0330

    add json tags to user model

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type User struct {
-	ID              int
-	Name            string
-	Email           string
-	Phone           string
-	Password        string
-	EmailVerifiedAt time.Time
-	PhoneVerifiedAt time.Time
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
+	ID              int       `json:"id"`
+	Name            string    `json:"name"`
+	Email           string    `json:"email"`
+	Phone           string    `json:"phone"`
+	Password        string    `json:"password"`
+	EmailVerifiedAt time.Time `json:"emailVerified_at"`
+	PhoneVerifiedAt time.Time `json:"phoneVerified_at"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
 }
