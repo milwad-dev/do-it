b440e6305c38b9db12bf15a573ce3fa2573d6457
Milwad <milwad.dev@gmail.com>
2024-04-26 18:01:33 +0330

    load envs

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	// Load environments
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// Port
 	port := fmt.Sprintf(":%v", os.Getenv("APP_PORT"))
@@ -21,7 +27,7 @@ func main() {
 
 	// Serve application
 	fmt.Printf("Your application run on %v", port)
-	err := http.ListenAndServe(port, router)
+	err = http.ListenAndServe(port, router)
 	if err != nil {
 		log.Fatal(err)
 	}
