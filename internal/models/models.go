f503e91845b7cc12dbe77aa3bc378afa86c6aa0e
Milwad <milwad.dev@gmail.com>
2024-04-26 17:47:39 +0330

    add User model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,15 @@
 package models
+
+import "time"
+
+type User struct {
+	ID              int
+	Name            string
+	Email           string
+	Phone           string
+	Password        string
+	EmailVerifiedAt time.Time
+	PhoneVerifiedAt time.Time
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
