f92c39fa37a3be96731ba64a9095bc0f7f61f27a
Milwad <milwad.dev@gmail.com>
2024-04-26 17:52:12 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	// Port
+	port := fmt.Sprintf(":%v", os.Getenv("APP_PORT"))
+
+	// Router
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("milwad"))
+	})
+
+	// Serve application
+	fmt.Printf("Your application run on %v", port)
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
