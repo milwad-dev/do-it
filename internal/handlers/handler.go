37edd5bb974c35d781851d258073548a6ae30376
Milwad <milwad.dev@gmail.com>
2024-04-27 18:11:27 +0330

    Create handler.go

---
+++ internal/handlers/handler.go
@@ -0,0 +1,12 @@
+package handlers
+
+import "database/sql"
+
+type DBHandler struct {
+	db *sql.DB
+}
+
+// NewDBHandler => create a new instance from DBHandler and return it
+func NewDBHandler(db *sql.DB) *DBHandler {
+	return &DBHandler{db: db}
+}
