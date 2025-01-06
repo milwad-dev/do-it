0bbdaadb2bfd5054001476e6b71689e74628b874
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-01-06 21:55:19 +0330

    add jwt service

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/milwad-dev/do-it
 go 1.22
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/joho/godotenv v1.5.1
---
+++ internal/services/jwt_service.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var secretKey = []byte("secretpassword")
+
+// GenerateToken generates a JWT token with the user ID as part of the claims
+func GenerateToken(userID uint) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["user_id"] = userID
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() // Token valid for 1 hour
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
+
+// VerifyToken verifies a token JWT validate
+func VerifyToken(tokenString string) (jwt.MapClaims, error) {
+	// Parse the token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	// Check for errors
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate the token
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
+}
