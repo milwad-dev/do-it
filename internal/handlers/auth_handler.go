0e096d8b1e213bb0c60598b78713065e3c08e763
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-01-10 17:39:41 +0330

    complete register

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/milwad-dev/do-it/internal/services"
 	"github.com/milwad-dev/do-it/internal/utils"
 	"log"
@@ -8,21 +10,42 @@ import (
 	"net/mail"
 )
 
-func (db *DBHandler) RegisterAuth(w http.ResponseWriter, r *http.Request) {
-	name := r.Form.Get("name")
-	username := r.Form.Get("username") // Email or Phone
-	password := r.Form.Get("password")
+type user struct {
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
+func (db *DBHandler) RegisterAuth(w http.ResponseWriter, r *http.Request) {
+	var user user
+
+	data := make(map[string]any)
+	// Parse body
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		data["message"] = err.Error()
+
+		utils.JsonResponse(w, data, 400)
+		return
+	}
+
+	// User info
+	name := user.Name
+	username := user.Username // Email or phone
+	password := user.Password
+
+	// Detect username
 	usernameField := detectEmailOrPhone(username)
 
-	rows, err := db.Query("SELECT count(*) FROM users WHERE ? = ?", usernameField, username)
+	// Check user not exists in db
+	queryExist := fmt.Sprintf("SELECT count(*) FROM users WHERE %s = ?", usernameField)
+	rows, err := db.Query(queryExist, username)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
 	var count int
-	var data map[string]any
 
 	for rows.Next() {
 		if err := rows.Scan(&count); err != nil {
@@ -35,26 +58,31 @@ func (db *DBHandler) RegisterAuth(w http.ResponseWriter, r *http.Request) {
 		data["message"] = "The user already exists."
 
 		utils.JsonResponse(w, data, 302)
+		return
 	}
 
 	// Hash password
 	hashPassword, _ := services.HashPassword(password)
 
 	// Create user
-	result, err := db.Exec("INSERT INTO users (name, ?, password) VALUES (?, ?, ?)", usernameField, name, username, hashPassword)
-	if err != nil {
-		data["message"] = "Problem on creating user."
+	query := fmt.Sprintf("INSERT INTO users (name, %s, password) VALUES (?, ?, ?)", usernameField)
+	result, errInsert := db.Exec(query, name, username, hashPassword)
+	if errInsert != nil {
+		data["message"] = fmt.Sprintf("Problem on creating user: %s", errInsert.Error())
 
 		utils.JsonResponse(w, data, 302)
+		return
 	}
 
 	// Create token
+	fmt.Println(name, username, password, usernameField, count)
 	userId, _ := result.LastInsertId()
-	token, err := services.GenerateToken(uint(userId))
-	if err != nil {
+	token, errToken := services.GenerateToken(uint(userId))
+	if errToken != nil {
 		data["message"] = "Problem on generating token."
 
 		utils.JsonResponse(w, data, 302)
+		return
 	}
 
 	data["message"] = "Register completed."
